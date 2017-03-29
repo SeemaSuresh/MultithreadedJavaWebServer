4edbc730a7b16c11448ea48e092ccff0d867d9ef
SeemaSuresh <seemamithanthaya@gmail.com>
2017-03-28 19:54:23 -0700

    Update HttpRequest.java

--- src/HttpRequest.java
+++ src/HttpRequest.java
@@ -10,6 +10,7 @@ public class HttpRequest
 	public HttpRequest(String httpReq)
 	{
 		// TODO Auto-generated constructor stub
+		//Get the file name from the http header request and call http response by passing the file name as parameter to the constuctor
 		String newLine[] = httpReq.split("\n"); 
 		System.out.println("HTTP Request GET: \n" + newLine[0]);
 		file = newLine[0].split(" ")[1];
